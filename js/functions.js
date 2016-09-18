c1c7f7a703c84fcb4658fec62c987965ad922c43
Abid Rahman <arahman1@ualberta.ca>
2016-09-18 14:28:29 -0600

    Fixed things, temporary complete website (hopefully).

--- js/functions.js
+++ js/functions.js
@@ -7,8 +7,10 @@ $(document).scroll(function() {
 		// user scrolled 50 pixels or more;
 		// do stuff
 		$(".navi_fade").fadeIn(700);
+    $(".header").fadeIn(700);
 	  }
 	  else {
+     $(".header").fadeOut(300);
 		 $(".navi_fade").fadeOut(300);
 	  }
 });
@@ -19,3 +21,19 @@ $(document).ready(function() {
 	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 1200);
 	});
 });
+
+
+
+//$(document).ready(function() {	
+//	var windowHeight = $(window).height();
+//			
+//	$(".contact_height").css('height', windowHeight),
+//	$("#body").css('marginBottom', windowHeight);
+//});
+//
+//$(window).resize(function() {	
+//	var windowHeight = $(window).height();
+//			
+//	$(".contact_height").css('height', windowHeight),
+//	$("#body").css('marginBottom', windowHeight);
+//});
